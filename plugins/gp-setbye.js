414dcd70e502c7cb9da326885ec9aeb4ea1997f6
laboratiork25 <githubunityy@gmail.com>
2025-09-21 19:14:40 +0000

    u

--- plugins/gp-setbye.js
+++ plugins/gp-setbye.js
@@ -1,2 +1,11 @@
-import '../lib/language.js';
-
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+if (text) {
+global.db.data.chats[m.chat].sBye = text
+m.reply('𝐀𝐝𝐝𝐢𝐨 𝐬𝐞𝐭𝐭𝐚𝐭𝐨 𝐜𝐨𝐫𝐫𝐞𝐭𝐭𝐚𝐦𝐞𝐧𝐭𝐞 ✅')
+} else throw `inserisci il messaggio`
+}
+handler.help = ['setbye <text>']
+handler.tags = ['group']
+handler.command = ['setbye'] 
+handler.admin = true
+export default handler
