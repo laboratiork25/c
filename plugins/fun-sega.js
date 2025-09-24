93f99d4ae290631229a03379996c0ab6590bc72b
laboratiork25 <githubunityy@gmail.com>
2025-09-24 21:43:51 +0200

    Update fun-sega.js

--- plugins/fun-sega.js
+++ plugins/fun-sega.js
@@ -1,8 +1,7 @@
 import { performance } from 'perf_hooks'
-import '../lib/language.js';
 
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-  let nomeDelBot = global.db.data.nomedelbot || global.t('fap_default_bot_name', m.sender, null, null, {})
+let handler = async (m, { conn, text }) => {
+  let nomeDelBot = global.db.data.nomedelbot || `𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲`
 
   // Identifica il destinatario: risposto o menzionato
   let destinatario;
@@ -11,28 +10,24 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   } else if (m.mentionedJid && m.mentionedJid.length > 0) {
     destinatario = m.mentionedJid[0];
   } else {
-    return m.reply(global.t('fap_no_target', m.sender, null, null, { prefix: usedPrefix, command: command }));
+    return m.reply("Tagga qualcuno o rispondi a un messaggio per segarlo 😏");
   }
 
   let nomeDestinatario = `@${destinatario.split('@')[0]}`
 
   // Messaggio iniziale
   let { key } = await conn.sendMessage(m.chat, { 
-    text: global.t('fap_start', m.sender, null, null, { target: nomeDestinatario }),
+    text: `Ora sego ${nomeDestinatario}...`,
     mentions: [destinatario]
   }, { quoted: m })
 
   const array = [
-    global.t('fap_animation_1', m.sender, null, null, {}),
-    global.t('fap_animation_2', m.sender, null, null, {}),
-    global.t('fap_animation_3', m.sender, null, null, {}),
-    global.t('fap_animation_4', m.sender, null, null, {}),
-    global.t('fap_animation_5', m.sender, null, null, {})
+    "8===👊=D", "8=👊===D", "8==👊==D", "8===👊=D", "8===👊=D💦"
   ]
 
   for (let item of array) {
     await conn.sendMessage(m.chat, { 
-      text: item, 
+      text: `${item}`, 
       edit: key,
       mentions: [destinatario]
     }, { quoted: m })
@@ -41,7 +36,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
   // Messaggio finale
   return conn.sendMessage(m.chat, { 
-    text: global.t('fap_finish', m.sender, null, null, { target: nomeDestinatario }),
+    text: `Oh ${nomeDestinatario} ha sborrato! 😋💦`,
     edit: key,
     mentions: [destinatario]
   }, { quoted: m })
@@ -49,6 +44,6 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 handler.help = ['sega']
 handler.tags = ['fun']
-handler.command = /^(sega|fap|masturbate)$/i
+handler.command = /^(sega)$/i
 
 export default handler
