4788772ada28e2c25a89d32aef20a774ea32aa6e
laboratiork25 <githubunityy@gmail.com>
2025-10-25 17:10:37 +0200

    Update gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -14,11 +14,10 @@ END:VCARD`.replace(/\n/g, '\r\n');
         contacts: { 
             displayName: '👥 ChatUnity Staff', 
             contacts: [
-                { vcard: createVCard('Creatore', '393515533859', 'Founder') },
-                { vcard: createVCard('co-founder', '639107484127', 'co-founder') },
+                { vcard: createVCard('Creatore', '393773842461', 'Founder') },
+                { vcard: createVCard('co-founder', '393793178438', 'co-founder') },
                 { vcard: createVCard('Finanziatore', '393803482529', 'Financer') },
                 { vcard: createVCard('SMM & MODDER', '393929139611', 'SMM & MODDER') },
-                { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
                 { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
                 { vcard: createVCard('Developer 6', '393509368693', 'Developer') },
                 { vcard: createVCard('Developer 7', '66621409462', 'Developer') },
