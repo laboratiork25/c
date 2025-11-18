a084adf7508d7e68694e24ce5685ddd4acc30415
AxtralWiZaRd <134928023+AxtralWiZaRd@users.noreply.github.com>
2025-11-18 01:22:39 +0100

    Update staff contacts in gp-staff.js

--- plugins/gp-staff.js
+++ plugins/gp-staff.js
@@ -14,16 +14,14 @@ END:VCARD`.replace(/\n/g, '\r\n');
         contacts: { 
             displayName: '👥 ChatUnity Staff', 
             contacts: [
-                { vcard: createVCard('Creatore', '393515533859', 'Founder') },
-                { vcard: createVCard('co-founder', '639107484127', 'co-founder') },
+                { vcard: createVCard('Creatore', '393773842461', 'Founder') },
+                { vcard: createVCard('co-founder', '393509368693', 'co-founder') },
                 { vcard: createVCard('Finanziatore', '393803482529', 'Financer') },
                 { vcard: createVCard('SMM & MODDER', '393929139611', 'SMM & MODDER') },
-                { vcard: createVCard('Developer 1', '393935731102', 'Developer') },
-                { vcard: createVCard('Developer 4', '393518419909', 'Developer') },
-                { vcard: createVCard('Developer 6', '393509368693', 'Developer') },
-                { vcard: createVCard('Developer 7', '66621409462', 'Developer') },
-                { vcard: createVCard('Developer 9', '393512884684', 'Developer') },
-                { vcard: createVCard('Developer 10', '447449775153', 'Developer') }
+                { vcard: createVCard('Developer 1', '66621409462', 'Developer') },
+                { vcard: createVCard('Developer 2', '393512884684', 'Developer') },
+                { vcard: createVCard('Developer 3', '393472425001', 'Developer') },
+                { vcard: createVCard('Developer 4', '393534409026', 'Developer') }
             ]
         }
     }, { quoted: m });
