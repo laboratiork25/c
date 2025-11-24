19364585b2b2bc6bba4a3dd7b7fc9bd1f1c6157e
laboratiork25 <githubunityy@gmail.com>
2025-11-24 11:25:18 +0100

    Update fun-drogato.js

--- plugins/fun-drogato.js
+++ plugins/fun-drogato.js
@@ -47,8 +47,27 @@ let handler = async (m, { conn, command, text }) => {
   );
 };
 
-handler.command = /^(drogato|drugtest)$/i;
+handler.command = /^(drogato|drugtest|drogadicto|pruebadrogas|drogado|testedrogas|drogenschnelltest|drogi_test|毒品测试|наркотест|drug_test_ar|مخدرات_اختبار|ड्रगटेस्ट|testdrogue|testnarkoba|uyuşturucutesti)$/i;
+
+handler.help = [
+  'drogato [@tag | nome]',
+  'drugtest [@tag | name]',
+  'drogadicto [@tag | nombre]',
+  'pruebadrogas [@tag | nombre]',
+  'drogado [@tag | nome]',
+  'testedrogas [@tag | nome]',
+  'drogenschnelltest [@tag | name]',
+  'drogi_test [@tag | name]',
+  '毒品测试 [@tag | 名字]',
+  'наркотест [@tag | имя]',
+  'drug_test_ar [@tag | اسم]',
+  'مخدرات_اختبار [@tag | اسم]',
+  'ड्रगटेस्ट [@tag | नाम]',
+  'testdrogue [@tag | nom]',
+  'testnarkoba [@tag | nama]',
+  'uyuşturucutesti [@tag | isim]'
+];
+
 handler.tags = ['fun'];
-handler.help = ['drogato [@tag | nome]', 'drugtest [@tag | name]'];
 
 export default handler;
