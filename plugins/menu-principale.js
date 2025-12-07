5ceef4536bfbd4bd04096cc8066b67891fe66cc3
mattedffdf <porcodioharam@gmail.com>
2025-12-08 00:28:54 +0100

    Update menu-principale.js

--- plugins/menu-principale.js
+++ plugins/menu-principale.js
@@ -1,67 +1,64 @@
-import { performance } from 'perf_hooks';
-import fetch from 'node-fetch';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import '../lib/language.js';
+import { performance } from 'perf_hooks'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import '../lib/language.js'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const handler = async (message, { conn, usedPrefix, command }) => {
-    const userId = message.sender;
-    const groupId = message.isGroup ? message.chat : null;
-    const userCount = Object.keys(global.db.data.users).length;
-    const botName = global.db.data.nomedelbot || 'ChatUnity';
+const handler = async (m, { conn, usedPrefix }) => {
+    const userId = m.sender
+    const groupId = m.isGroup ? m.chat : null
+    const userCount = Object.keys(global.db.data.users).length
+    const botName = global.db.data.nomedelbot || 'ChatUnity'
 
-    const menuText = generateMenuText(usedPrefix, botName, userCount, userId, groupId);
-    const videoPath = path.join(__dirname, '../media/principale.gif');
+    const menuText = generateMenuText(usedPrefix, botName, userCount, userId, groupId)
+    const imagePath = path.join(__dirname, '../media/principale.jpg')
 
-    const footerText = global.t('menuFooter', userId, groupId);
-    const adminMenuText = global.t('menuAdmin', userId, groupId);
-    const ownerMenuText = global.t('menuOwner', userId, groupId);
-    const securityMenuText = global.t('menuSecurity', userId, groupId);
-    const groupMenuText = global.t('menuGroup', userId, groupId);
-    const aiMenuText = global.t('menuAI', userId, groupId);
+    const footerText = global.t('menuFooter', userId, groupId)
+    const adminMenuText = global.t('menuAdmin', userId, groupId)
+    const ownerMenuText = global.t('menuOwner', userId, groupId)
+    const securityMenuText = global.t('menuSecurity', userId, groupId)
+    const groupMenuText = global.t('menuGroup', userId, groupId)
+    const aiMenuText = global.t('menuAI', userId, groupId)
 
     const buttons = [
-        { quickReplyButton: { displayText: adminMenuText, id: `${usedPrefix}menuadmin` } },
-        { quickReplyButton: { displayText: ownerMenuText, id: `${usedPrefix}menuowner` } },
-        { quickReplyButton: { displayText: securityMenuText, id: `${usedPrefix}menusicurezza` } },
-        { quickReplyButton: { displayText: groupMenuText, id: `${usedPrefix}menugruppo` } },
-        { quickReplyButton: { displayText: aiMenuText, id: `${usedPrefix}menuia` } }
-    ];
+        { buttonId: `${usedPrefix}menuadmin`, buttonText: { displayText: adminMenuText }, type: 1 },
+        { buttonId: `${usedPrefix}menuowner`, buttonText: { displayText: ownerMenuText }, type: 1 },
+        { buttonId: `${usedPrefix}menusicurezza`, buttonText: { displayText: securityMenuText }, type: 1 },
+        { buttonId: `${usedPrefix}menugruppo`, buttonText: { displayText: groupMenuText }, type: 1 },
+        { buttonId: `${usedPrefix}menuia`, buttonText: { displayText: aiMenuText }, type: 1 }
+    ]
 
-    const msg = {
-        video: { url: videoPath },
+    await conn.sendMessage(m.chat, {
+        image: { url: imagePath },
         caption: menuText,
         footer: footerText,
-        templateButtons: buttons
-    };
+        buttons,
+        headerType: 4
+    }, { quoted: m })
+}
 
-    await conn.sendMessage(message.chat, msg, { quoted: message });
-};
+handler.help = ['menu']
+handler.tags = ['menu']
+handler.command = /^(menu|comandi|commands)$/i
 
-handler.help = ['menu'];
-handler.tags = ['menu'];
-handler.command = /^(menu|comandi|commands)$/i;
-
-export default handler;
+export default handler
 
 function generateMenuText(prefix, botName, userCount, userId, groupId) {
-    const menuTitle = global.t('mainMenuTitle', userId, groupId);
-    const staffText = global.t('staffCommand', userId, groupId);
-    const candidatesText = global.t('candidatesCommand', userId, groupId);
-    const installText = global.t('installCommand', userId, groupId);
-    const channelsText = global.t('channelsCommand', userId, groupId);
-    const systemText = global.t('systemCommand', userId, groupId);
-    const pingText = global.t('pingCommand', userId, groupId);
-    const reportText = global.t('reportCommand', userId, groupId);
-    const suggestText = global.t('suggestCommand', userId, groupId);
-    const versionText = global.t('versionLabel', userId, groupId);
-    const collabText = global.t('collabLabel', userId, groupId);
-    const usersText = global.t('usersLabel', userId, groupId);
-    const vs = global.vs || '8.0';
+    const menuTitle = global.t('mainMenuTitle', userId, groupId)
+    const staffText = global.t('staffCommand', userId, groupId)
+    const candidatesText = global.t('candidatesCommand', userId, groupId)
+    const installText = global.t('installCommand', userId, groupId)
+    const channelsText = global.t('channelsCommand', userId, groupId)
+    const systemText = global.t('systemCommand', userId, groupId)
+    const pingText = global.t('pingCommand', userId, groupId)
+    const reportText = global.t('reportCommand', userId, groupId)
+    const suggestText = global.t('suggestCommand', userId, groupId)
+    const versionText = global.t('versionLabel', userId, groupId)
+    const collabText = global.t('collabLabel', userId, groupId)
+    const usersText = global.t('usersLabel', userId, groupId)
+    const vs = global.vs || '8.0'
 
     return `
 ⋆ ︵★ ${menuTitle} ★︵ ⋆
@@ -80,5 +77,5 @@ function generateMenuText(prefix, botName, userCount, userId, groupId) {
   ୧・${collabText}
   ୧・${usersText}: ${userCount}
 ╰♡꒷ ๑ ⋆˚₊⋆──ʚ˚ɞ──⋆˚₊⋆ ๑ ⪩
-`.trim();
+`.trim()
 }
